8dd56e7965d064a3836b02696eabf700a7c3c03c
PieroVicenti <p.vicenti@outlook.com>
2022-01-09 22:15:39 +0000

    Updates

---
+++ precache-manifest.8fb66a3cc03d3fddb92f1bb6a38c48d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7d06e1f1346d1c534f982f383b40c8",
+    "url": "/Reviews/index.html"
+  },
+  {
+    "revision": "2bd444ad2f52fe670658",
+    "url": "/Reviews/static/css/main.7c3efe9a.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd444ad2f52fe670658",
+    "url": "/Reviews/static/js/main.a06f1c7c.chunk.js"
+  },
+  {
+    "revision": "5cfec122a862e1e7b69a",
+    "url": "/Reviews/static/js/runtime-main.949f2259.js"
+  }
+]);
